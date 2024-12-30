bc02de34fee4c96661fdc33ff82e1fc14f57dab4
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-30 18:57:34 +0100

    ngOnDestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -10,15 +11,23 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
 
+  public interval$?: Subscription;
+
   ngOnInit(): void {
     console.log("Componente HIJO: ngOnInit");
+
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${value}`))
   }
+
   ngOnChanges(changes: SimpleChanges): void {
     console.log("Componente HIJO: ngOnChanges");
     console.log({changes});
   }
+
   ngOnDestroy(): void {
     console.log("Componente HIJO: ngOnDestroy");
+
+    this.interval$?.unsubscribe();
   }
 
 }
